587e034d94c048c673b7d69fb2d92b366b933f70
Shubham Phapale <shubhamphapale10@gmail.com>
2024-08-16 13:55:38 +0530

    Added Global Descriptor Table

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,5 @@
 #include "types.h"
+#include "gdt.h"
 
 void printf(const char* str)
 {
@@ -21,5 +22,7 @@ extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
     printf("Hello World!");
 
+    GlobalDescriptorTable gdt;
+    
     while(1);
 }
