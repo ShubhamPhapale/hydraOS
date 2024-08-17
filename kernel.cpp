a951e97a5106b52910d8c8c5adb3b976aff7ce5c
Shubham Phapale <shubhamphapale10@gmail.com>
2024-08-17 20:54:12 +0530

    Interrupts:First Response from Hardware

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,6 @@
 #include "types.h"
 #include "gdt.h"
+#include "interrupts.h"
 
 void printf(const char* str)
 {
@@ -53,6 +54,9 @@ extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
     printf("This is a new line!");
 
     GlobalDescriptorTable gdt;
+    InterruptManager interrupts(&gdt);
+
+    interrupts.Activate();
     
     while(1);
 }
