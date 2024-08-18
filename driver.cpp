7d895bc5c86f5c7b61ff0237f8ad248470512b17
Shubham Phapale <shubhamphapale10@gmail.com>
2024-08-18 15:14:38 +0530

    Driver Abstraction

---
+++ driver.cpp
@@ -0,0 +1,37 @@
+#include "driver.h"
+
+Driver::Driver() {
+    // Initialize the driver
+}
+
+Driver::~Driver() {
+    // Deinitialize the driver
+}
+
+void Driver::Activate() {
+    // Activate the driver
+}
+
+int Driver::Reset() {
+    // Reset the driver
+    return 0;
+}
+
+void Driver::Deactivate() {
+    // Deactivate the driver
+}
+
+DriverManager::DriverManager() {
+    numDrivers = 0;
+}
+
+void DriverManager::AddDriver(Driver* driver) {
+    drivers[numDrivers] = driver;
+    numDrivers++;
+}
+
+void DriverManager::ActivateAll() {
+    for (int i = 0; i < numDrivers; i++) {
+        drivers[i]->Activate();
+    }
+}
--- interrupts.cpp
+++ interrupts.cpp
@@ -1,6 +1,7 @@
 #include "interrupts.h"
 
-void printf(const char* str);
+void printf(const char*);
+void printfHex(uint8_t);
 
 InterruptHandler::InterruptHandler(uint8_t InterruptNumber, InterruptManager* interruptManager)
 {
@@ -163,11 +164,8 @@ uint32_t InterruptManager::DoHandleInterrupt(uint8_t interrupt, uint32_t esp)
     }
     else if (interrupt != hardwareInterruptOffset)
     {
-        char* foo = "UNHANDLED INTERRUPT 0x00";
-        char* hex = "0123456789ABCDEF";
-        foo[22] = hex[(interrupt >> 4) & 0xF];
-        foo[23] = hex[interrupt & 0xF];
-        printf(foo);
+        printf("UNHANDLED INTERRUPT 0x");
+        printfHex(interrupt);  
     }
 
     // hardware interrupts must be acknowledged
